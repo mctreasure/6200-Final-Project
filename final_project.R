099bc1f8f64866b6a964dd414fbd38ca31395636
Ernest <ekf98@hotmail.com>
2024-11-20 13:47:25 -0500

    initial start

---
+++ final_project.R
@@ -0,0 +1,4 @@
+website_links = list("https://www.metro.ca/en/online-grocery/aisles/fruits-vegetables",
+                      "https://www.zehrs.ca/food/fruits-vegetables/c/28000?navid=flyout-L2-fruits-vegetables",
+                      "https://www.walmart.ca/en/browse/grocery/fruits-vegetables/10019_6000194327370?icid=cp_l2_page_grocery_produce_shop_all_89673_FOPA0AUQJ3")
+
