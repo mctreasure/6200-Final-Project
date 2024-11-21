032c574ce78e06badf4f33199b3682feccdb8202
mctreasure <mtreasur@uoguelph.ca>
2024-11-21 15:17:56 -0500

    Added fuel_prices.R

---
+++ fuel_prices.R
@@ -0,0 +1,6 @@
+
+fuel_prices = list(https://natural-resources.canada.ca/our-natural-resources/domestic-and-international-markets/transportation-fuel-prices/4593
+                 
+)
+
+#this link has weekly/daily prices 
